6c19370e23a66ed37ea9601a71dc78b9b7b3f90c
tan4786 <99185190+tan4786@users.noreply.github.com>
2022-02-23 18:59:50 +0900

    Create index.rb

---
+++ index.rb
@@ -0,0 +1,31 @@
+require "csv"
+puts "1(新規でメモを作成) 2(既存のメモ編集する)"
+
+memo_type = gets.to_s
+
+if memo_type.to_i == 1
+  puts "拡張子を除いたファイルを入力してください"
+  
+  file_name = gets.to_s
+   puts "メモしたい内容を記入してください"
+   puts "完了したらctrl+Dをおします"
+   
+   CSV.open("{file_name}.csv",'w') do |csv|
+     memo_input = readlines
+     csv << memo_input
+   end
+   
+else memo_type.to_i == 2
+  puts "拡張子を除いたファイルを入力してください"
+  
+  file_name = gets.to_s
+   puts "編集したい内容を記入してください"
+   puts "完了したらctrl+Dをおします"
+   
+   CSV.open("{file_name}.csv",'a') do |csv|
+     memo_input = readlines
+     csv << memo_input
+   end
+   end
+
+
